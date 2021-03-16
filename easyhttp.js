66f8c4e2e7842b0c9ca69f02f093aec140278d85
Dpascuttini <dominik.pascuttini1@gmail.com>
2021-03-16 21:44:25 +0100

    .

--- easyhttp.js
+++ easyhttp.js
@@ -6,12 +6,30 @@ function easyHTTP() {
 easyHTTP.prototype.get = function (url) {
   this.http.open('GET', url, true);
 
-  this.http.onload = function () {};
+  let self = this;
+  this.http.onload = function () {
+    if (self.http.status === 200) {
+      callback(null, self.http.responseText);
+    } else {
+      callback('Error: ', +self.http.status);
+    }
+  };
 
   this.http.send();
 };
 
 // Make an HTTP POST Request
+easyHTTP.prototype.post = function (url, data, callback) {
+  this.http.open('POST', url, true);
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+  this.http.onload = function () {
+    callback(null, self.http.responseText);
+  };
+
+  this.send(JSON.stringify(data));
+};
 
 // Make an HTTP PUT Request
 
